682750c4c72597529f27ace7fe812ef32d0923b8
hafzl <107129895+hafzl@users.noreply.github.com>
2023-05-29 20:11:34 +0700

    Update selesai.php

--- selesai.php
+++ selesai.php
@@ -4,7 +4,7 @@
 
 <div class="container" style="padding-bottom: 300px;">
 	<h2 class="bg-success text-center" style="padding: 10px;">Checkout Berhasil</h2>
-	<h4 class="text-center" style="font-weight: bold;">Terimakasih telah berbelanja di Rapi-Cake Backery, pesananmu sedang diproses silahkan tunggu barangmu di rumah ya.. :) jangan di rumah tetangga, nanti kami susah lho nyarinya, hehe...</h4>
+	<h4 class="text-center" style="font-weight: bold;">Terimakasih telah berbelanja di Nur Food's Cake & Cookies, pesananmu sedang diproses silahkan tunggu barangmu di rumah ya.. :) jangan di rumah tetangga, nanti kami susah lho nyarinya, hehe...</h4>
 	
 </div>
 
