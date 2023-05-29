7f21f8bf8caa73bb15e5155bf2570238fef1d865
aivetaa <122965618+aivetaa@users.noreply.github.com>
2023-05-29 20:18:59 +0700

    Update user_login.php

--- user_login.php
+++ user_login.php
@@ -2,18 +2,18 @@
 	include 'header.php';
  ?>
 
-<div class="container" style="padding-bottom: 250px;">
-		<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Login</b></h2>
+<div class="container" style="padding-bottom: 255px;">
+		<h2 style=" width: 100%; border-bottom: 4px solid #CD853F"><b>Login</b></h2>
 
 <form action="proses/login.php" method="POST">
 		<div class="form-group">
-			<label for="exampleInputEmail1">username</label>
-			<input type="text" class="form-control" id="exampleInputEmail1" placeholder="Username" name="username" style="width: 500px;">
+			<label for="exampleInputEmail1">Username</label>
+			<input type="text" class="form-control" id="exampleInputEmail1" placeholder="Username" name="username" style="width: 550px;">
 		</div>
 		
 		<div class="form-group">
 			<label for="exampleInputEmail1">Password</label>
-			<input type="password" class="form-control" id="exampleInputEmail1" placeholder="Password" name="pass" style="width: 500px;">
+			<input type="password" class="form-control" id="exampleInputEmail1" placeholder="Password" name="pass" style="width: 550px;">
 		</div>
 		<button type="submit" class="btn btn-success">Login</button>
 		<a href="register.php" class="btn btn-primary">Daftar</a>
