bcca1f42b3f4107a30ea1203a6f0ef20dce9445f
Ikryz7 <130422422+Ikryz7@users.noreply.github.com>
2023-05-29 19:39:16 +0700

    Update footer.php
    update

--- footer.php
+++ footer.php
@@ -3,27 +3,12 @@
 		<div class="container" style="padding-bottom: 50px;">
 			<div class="row">
 				<div class="col-md-4">
-					<h3 style="color: #ff8680"><b>RAPI-CAKE BAKERY</b></h3>
-					<p>Jl. Tanah Merah Indah 1 No.10C</p>
-					<p><i class="glyphicon glyphicon-earphone"></i> +6287804616097</p>
-					<p><i class="glyphicon glyphicon-envelope"></i> o-cake@gmail.com</p>
-				</div>
-				<div class="col-md-4">
-					<h5><b>Menu</b></h5>
-					<p><a href=""  style="color: #000">Produk</a></p>
-					<p><a href=""  style="color: #000">Tentang kami</a></p>
-					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
-				</div>
-
-				<div class="col-md-4">
-					
+					<h3 style="color: #ff8680"><b>Nur Food's Cake & Cookies</b></h3>
+					<p><a href="https://www.google.com/maps/place/Jl.+Griya+Cempaka+Arum,+Kec.+Gedebage,+Kota+Bandung,+Jawa+Barat/@-6.9555267,107.7092577,18.99z/data=!4m6!3m5!1s0x2e68c2e4cd380f95:0xc5e24c589f46d6f9!8m2!3d-6.9553165!4d107.7083837!16s%2Fg%2F11bx230p26?hl=id&entry=ttu"  style="color: #000"> Jl. Griya Cempaka Arum, Kec. Gedebage, Kota Bandung, Jawa Barat, Indonesia</a></p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +62 823-1834-0912</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> nurfood@gmail.com</p>
 				</div>
 			</div>
-
-		</div>
-
-		<div class="copy" style="background-color: #ff8680; padding: 5px; color: #fff; text-align: center;">
-			<span>Copyright&copy; Ahmad Rafi Akbar Putra Hamzah</span>
 		</div>
 	</footer>
 
