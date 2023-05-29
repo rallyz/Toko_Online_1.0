d40a74849a8bb8351c461556f22e23c97c603d4b
aivetaa <122965618+aivetaa@users.noreply.github.com>
2023-05-29 20:22:37 +0700

    Update register.php

--- register.php
+++ register.php
@@ -23,13 +23,13 @@ include 'header.php';
 		<div class="row">
 			<div class="col-md-6">
 				<div class="form-group">
-					<label for="exampleInputPassword1">username</label>
+					<label for="exampleInputPassword1">Username</label>
 					<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Username" name="username" required >
 				</div>
 			</div>
 			<div class="col-md-6">
 				<div class="form-group">
-					<label for="exampleInputPassword1">No Tepl</label>
+					<label for="exampleInputPassword1">No Telepon</label>
 					<input type="text" class="form-control" id="exampleInputPassword1" placeholder="+62" name="telp" required >
 				</div>
 			</div>
